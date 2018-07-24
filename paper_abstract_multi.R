28701e6d865620ebce29187afe34aa88d91cc8f5
Alexander Florez Rodriguez <alexflorezr@gmail.com>
2018-07-24 18:13:01 +0200

    Paper abstract multi using 3 gru-rnn.

--- paper_abstract_multi.R
+++ paper_abstract_multi.R
@@ -7,21 +7,21 @@
 #       Molecular ecology
 ## ---- upload abstracts ----
 root <- "~/Desktop/A/Me/Papers_abstracts"
-journals_dir <- dir(root)
 # file connection
 # abstract parser reads the text files and create the numeric labels 
 abstract_parser <- function(dir_path){
         all_journal <- list()
         journals_dir <- dir(dir_path)
-        attr(journals_dir, "names") <- dir(dir_path)
+        #attr(journals_dir, "names") <- dir(dir_path)
         label_count <- 1
         for (j in 1:length(journals_dir)){
                 file_dir <- file.path(dir_path, journals_dir[j])
                 file_name <- dir(file_dir, pattern="*abstract.txt")
-                data <- strsplit(file_name, split="_")[[1]][1]
-                assign(data,readLines(file.path(file_dir, file_name)))
-                all_journal[[(j*2)-1]] <- get(data)
-                all_journal[[j*2]] <- rep(j, length(get(data)))
+                all_journal[[(j*2)-1]] <- readLines(file.path(file_dir, file_name), encoding = "UTF-8")
+                # data <- strsplit(file_name, split="_")[[1]][1]
+                # assign(data,readLines(file.path(file_dir, file_name), encoding = "UTF-8"))
+                # all_journal[[(j*2)-1]] <- get(data)
+                all_journal[[j*2]] <- rep(j, length(all_journal[[(j*2)-1]]))
         }
         all_journal
 }
@@ -58,6 +58,81 @@ segment_data <- function(abstract_list, perc_test, perc_val, seed){
         all_data
 }
 all_segmented <- segment_data(all_journal, .25, .25, 55)
-str(all_segmented)
-str(all_segmented[[1]])
 
+extract_data_label <- function(data, type){
+        if(type == "data"){
+        tmp <- data[seq(1,(length(data))-1,2)]
+        }
+        if(type == "label"){
+        tmp <- data[seq(2,length(data),2)]
+        }
+        out <- unlist(tmp)
+}
+train <- all_segmented[[1]]
+        train_data <- extract_data_label(train, "data")
+        train_label <- extract_data_label(train, "label")
+val  <- all_segmented[[2]]
+        val_data <- extract_data_label(val, "data")
+        val_label <- extract_data_label(val, "label")
+test <- all_segmented[[3]]
+        test_data <- extract_data_label(test, "data")
+        test_label <- extract_data_label(test, "label")
+## ---- tokenization ----
+maxlen <- 200
+max_words <- 10000
+
+train_val <- c(train_data, val_data)
+tokenizer <- text_tokenizer(num_words = max_words) %>% 
+        fit_text_tokenizer(train_val)
+word_index <- tokenizer$word_index
+# training sequences
+train_sequences <- texts_to_sequences(tokenizer, train_data)
+train_x <- pad_sequences(train_sequences, maxlen =  maxlen)
+train_y <-  to_categorical(train_label)
+# validation sequences
+val_sequences <- texts_to_sequences(tokenizer, val_data)
+val_x <- pad_sequences(val_sequences, maxlen)
+val_y <- to_categorical(val_label)
+## ---- embedding with rnn ----
+# model
+model <- keras_model_sequential() %>% 
+        layer_embedding(input_dim = 10000, output_dim = 32, input_length = maxlen) %>% 
+        layer_simple_rnn(units=32, activation = "relu") %>% 
+        layer_dense(units=7, activation="softmax")
+# compiler
+model %>% compile(
+        loss= "categorical_crossentropy",
+        metrics = c("accuracy"),
+        optimizer = "rmsprop"
+)
+# fit
+history <- model %>% fit(
+        train_x, 
+        train_y, 
+        epochs = 10, 
+        batch_size = 32, 
+        validation_data = list(val_x, val_y)
+)
+## ---- embedding with gru ----
+# model
+model_gru <- keras_model_sequential() %>% 
+        layer_embedding(input_dim = 10000, output_dim = 32, input_length = maxlen) %>% 
+        layer_gru(units=32, activation = "relu", return_sequences = T) %>%
+        layer_gru(units=64, activation = "relu", dropout = .5, recurrent_dropout = .3, 
+                  return_sequences = T) %>% 
+        layer_gru(units=128, activation = "relu", dropout = .2, recurrent_dropout = .1) %>% 
+        layer_dense(units=7, activation="softmax")
+# compiler
+model_gru %>% compile(
+        loss= "categorical_crossentropy",
+        metrics = c("accuracy"),
+        optimizer = "rmsprop"
+)
+# fit
+history_gru <- model_gru %>% fit(
+        train_x, 
+        train_y, 
+        epochs = 10, 
+        batch_size = 32, 
+        validation_data = list(val_x, val_y)
+)
