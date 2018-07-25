0b2dcfdec0f1a876a4e77dd8c8c71c7b4723dd5d
Alexander Florez Rodriguez <alexflorezr@gmail.com>
2018-07-25 18:49:51 +0200

    GloVe and long gru

--- paper_abstract_multi.R
+++ paper_abstract_multi.R
@@ -58,7 +58,6 @@ segment_data <- function(abstract_list, perc_test, perc_val, seed){
         all_data
 }
 all_segmented <- segment_data(all_journal, .25, .25, 55)
-
 extract_data_label <- function(data, type){
         if(type == "data"){
         tmp <- data[seq(1,(length(data))-1,2)]
@@ -78,7 +77,7 @@ test <- all_segmented[[3]]
         test_data <- extract_data_label(test, "data")
         test_label <- extract_data_label(test, "label")
 ## ---- tokenization ----
-maxlen <- 200
+maxlen <- 300
 max_words <- 10000
 
 train_val <- c(train_data, val_data)
@@ -136,3 +135,80 @@ history_gru <- model_gru %>% fit(
         batch_size = 32, 
         validation_data = list(val_x, val_y)
 )
+plot(history_gru)
+## ---- using GloVe ----
+# using the pretrained embedding 
+glove_dir <- "~/Downloads/glove.6B/"
+lines <- readLines(file.path(glove_dir, "glove.6B.100d.txt"))
+embedding_index <- new.env(hash=T, parent=emptyenv())
+for(i in 1:length(lines)){
+        line <- lines[[i]]
+        values <- strsplit(line, " ")[[1]]
+        word <- values[[1]]
+        # as double simple trasform the character values to numbers
+        embedding_index[[word]] <- as.double(values[-1])
+}
+embedding_dim <- 100
+embedding_matrix <- array(0, c(max_words, embedding_dim))
+# assign an embedding vector to each of the words in the index
+for(word in names(word_index)){
+        index <- word_index[[word]]
+        if (index < max_words){
+                embedding_vector <- embedding_index[[word]]
+                if(!is.null(embedding_vector)){
+                        embedding_matrix[index+1,] <- embedding_vector
+                }
+        }
+}
+# define the model
+model_glove <- keras_model_sequential() %>% 
+        layer_embedding(input_dim = 10000, output_dim = embedding_dim,
+                        input_length = maxlen) %>% 
+        layer_gru(units=32, activation = "relu", return_sequences = T) %>%
+        layer_gru(units=64, activation = "relu", dropout = .5, recurrent_dropout = .3) %>%
+        layer_dense(units=7, activation="softmax")
+# get the embedding layer and change the weigths for Glove embeddings
+get_layer(model_glove, index = 0) %>% 
+        set_weights(list(embedding_matrix)) %>% 
+        freeze_weights()
+# compilation scheme
+model_glove %>% compile(
+        loss = "categorical_crossentropy",
+        metrics = c("accuracy"),
+        optimizer = "rmsprop"
+)
+## ---- deeper gru ---- 
+# model
+model_gru_longer <- keras_model_sequential() %>% 
+        layer_embedding(input_dim = 10000, output_dim = 32, input_length = maxlen) %>% 
+        layer_gru(units=32, activation = "relu", return_sequences = T) %>%
+        layer_gru(units=64, activation = "relu", dropout = .5, recurrent_dropout = .3, 
+                  return_sequences = T) %>% 
+        layer_gru(units=128, activation = "relu", dropout = .2, recurrent_dropout = .1,
+                  return_sequences = T) %>% 
+        layer_gru(units=524, activation = "relu") %>% 
+        layer_dense(units=7, activation="softmax")
+# compiler
+model_gru_longer %>% compile(
+        loss= "categorical_crossentropy",
+        metrics = c("accuracy"),
+        optimizer = "rmsprop"
+)
+# fit
+history_gru_longer <- model_gru_longer %>% fit(
+        train_x, 
+        train_y, 
+        epochs = 20, 
+        batch_size = 32, 
+        validation_data = list(val_x, val_y)
+)
+history_glove <- model_glove %>% fit(
+        train_x, 
+        train_y, 
+        epochs = 10, 
+        batch_size = 32, 
+        validation_data = list(val_x, val_y)
+)
+## ---- 1d CNN ----
+
+## ---- ResNet ----
